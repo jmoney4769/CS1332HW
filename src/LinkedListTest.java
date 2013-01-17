ebac4175745b80e257b3b4e679c803b64f20de35
jmoney4769 <jaredmoore47@gmail.com>
2013-01-17 17:09:57 -0500

    Finished LinkedListTest

--- src/LinkedListTest.java
+++ src/LinkedListTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 import org.junit.Test;
 
@@ -58,19 +59,14 @@ public class LinkedListTest {
 	}
 
 	/**
-	 * Test method for {@link LinkedList#remove(int)}.
-	 */
-	@Test
-	public void testRemoveInt() {
-		fail("Not yet implemented");
-	}
-
-	/**
 	 * Test method for {@link LinkedList#remove(java.lang.Object)}.
 	 */
 	@Test
 	public void testRemoveObject() {
-		fail("Not yet implemented");
+		LinkedList<Integer> list = addElements();
+		assertEquals(new Integer(4), list.remove(new Integer(4)));
+		
+		assertEquals(null, list.remove(new Integer(12)));
 	}
 
 	/**
@@ -78,6 +74,137 @@ public class LinkedListTest {
 	 */
 	@Test
 	public void testSet() {
-		fail("Not yet implemented");
+		LinkedList<Integer> list = addElements();
+		assertEquals(list.get(4), list.set(4, new Integer(35)));
+	}
+	
+	/**
+	 * @author Adam Yost
+	 */
+	@Test
+	//@Worth(5) // these annotations are used by us to determine how much a method is worth
+	public void add() {
+		List<String> l1 = new LinkedList<>();
+		l1.add("A");
+		assertEquals("[ A ]", l1.toString());
+	}
+	
+	@Test
+	//@Worth(5)
+	public void addAll() {
+		List<String> l1 = new LinkedList<>(); 
+		Collection<String> c = new ArrayList<>();
+		c.add("A");
+		c.add("B");
+		c.add("C");
+		c.add("D");
+		l1.addAll(c);
+		//System.out.println(l1);
+		assertEquals("[ A B C D ]", l1.toString());
+	}
+
+	@Test
+	//@Worth(3)
+	public void clear() {
+		List<String> l1 = new LinkedList<>();
+		l1.add("A");
+		//System.out.println(l1);
+		l1.clear();
+		assertEquals("[ ]", l1.toString());
+	}
+
+	@Test
+	//@Worth(2)
+	public void contains() {
+		List<String> l1 = new LinkedList<>();
+		l1.add("A");
+		l1.add("B");
+		l1.add("C");
+		assertTrue(l1.contains("B"));
+	}
+	
+	@Test
+	//@Worth(3)
+	public void get() {
+		List<String> l1 = new LinkedList<>();
+		l1.add("A");
+		l1.add("B");
+		assertEquals("B", l1.get(1));
+	}
+	
+	@Test
+	//@Worth(3)
+	public void indexOf() {
+		List<String> l1 = new LinkedList<>();
+		l1.add("A");
+		l1.add("B");
+		assertEquals(1,l1.indexOf("B"));
+	}
+	
+	@Test
+	//@Worth(3)
+	public void isEmpty() {
+	List<String> l1 = new LinkedList<>();
+		assertTrue(l1.isEmpty());
+		l1.add("A");
+		l1.add("B");
+		assertFalse(l1.isEmpty());
+	}
+	
+	@Test
+	//@Worth(3)
+	public void removeindex() {
+	List<String> l1 = new LinkedList<>();
+		assertTrue(l1.isEmpty());
+		l1.add("A");
+		l1.add("B");
+		l1.add("C");
+		String e = l1.remove(1);
+		assertEquals("B",e);
+		assertEquals("[ A C ]", l1.toString());
+	}
+	
+	@Test
+	//@Worth(3)
+	public void remove2() {
+		List<String> l1 = new LinkedList<>();
+		l1.add("A");
+		l1.add("B");
+		l1.add("C");
+		String e = l1.remove("B");
+		assertEquals("B", e);
+		assertEquals("[ A C ]", l1.toString());
+	}
+
+	@Test
+	//@Worth(3)
+	public void set() {
+		List<String> l1 = new LinkedList<>();
+		l1.add("A");
+		l1.add("B");
+		l1.add("C");
+		l1.set(1, "E");
+		assertEquals("[ A E C ]", l1.toString());
+	}
+
+	@Test
+	//@Worth(3)
+	public void size() {
+		List<String> l1 = new LinkedList<>();
+		l1.add("A");
+		l1.add("B");
+		l1.add("C");
+		assertTrue(l1.size() == 3);
+	}
+
+	@Test
+	//@Worth(3)
+	public void setSize() {
+		LinkedList<String> l1 = new LinkedList<>();
+		l1.add("A");
+		l1.add("B");
+		l1.add("C");
+		l1.setSize(1);
+		assertTrue(l1.size()==1);
 	}
 }
