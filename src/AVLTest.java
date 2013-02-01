3cf07458fae07937e20c2db2605fa32b08b17e48
jmoney4769 <jaredmoore47@gmail.com>
2013-02-01 13:09:23 -0500

    Updated my part of the tests

--- src/AVLTest.java
+++ src/AVLTest.java
@@ -32,18 +32,18 @@ public class AVLTest {
 	@Test
 	public void testRightHeavy() {
 		AVL<Integer> tree = new AVL<Integer>();
-		tree.add(25); // right heavy case
+		tree.add(25);
 		tree.add(13);
 		tree.add(50);
 		tree.add(37);
 		tree.add(75);
 		tree.add(90);
-		assertEquals((int) tree.getRoot().getData(), 50);
-		assertEquals((int) tree.getRoot().getLeft().getData(), 25);
-		assertEquals((int) tree.getRoot().getRight().getData(), 75);
-		assertEquals((int) tree.getRoot().getLeft().getLeft().getData(), 13);
-		assertEquals((int) tree.getRoot().getLeft().getRight().getData(), 37);
-		assertEquals((int) tree.getRoot().getRight().getRight().getData(), 90);
+		assertEquals(50, (int) tree.getRoot().getData());
+		assertEquals(25, (int) tree.getRoot().getLeft().getData());
+		assertEquals(75, (int) tree.getRoot().getRight().getData());
+		assertEquals(13, (int) tree.getRoot().getLeft().getLeft().getData());
+		assertEquals(37, (int) tree.getRoot().getLeft().getRight().getData());
+		assertEquals(90, (int) tree.getRoot().getRight().getRight().getData());
 	}
 
 	@Test
@@ -53,15 +53,15 @@ public class AVLTest {
 		tree.add(75);
 		tree.add(90);
 		tree.add(50);
-		tree.add(37);
+		tree.add(60);
 		tree.add(25);
 		tree.add(13);
-		assertEquals((int) tree.getRoot().getData(), 37);
-		assertEquals((int) tree.getRoot().getLeft().getData(), 25);
-		assertEquals((int) tree.getRoot().getRight().getData(), 75);
-		assertEquals((int) tree.getRoot().getLeft().getLeft().getData(), 13);
-		assertEquals((int) tree.getRoot().getRight().getLeft().getData(), 50);
-		assertEquals((int) tree.getRoot().getRight().getRight().getData(), 90);
+		assertEquals(50, (int) tree.getRoot().getData());
+		assertEquals(25, (int) tree.getRoot().getLeft().getData());
+		assertEquals(75, (int) tree.getRoot().getRight().getData());
+		assertEquals(13, (int) tree.getRoot().getLeft().getLeft().getData());
+		assertEquals(60, (int) tree.getRoot().getRight().getLeft().getData());
+		assertEquals(90, (int) tree.getRoot().getRight().getRight().getData());
 	}
 
 	@Test
@@ -74,12 +74,12 @@ public class AVLTest {
 		tree.add(13);
 		tree.add(37);
 		tree.add(30);
-		assertEquals((int) tree.getRoot().getData(), 37);
-		assertEquals((int) tree.getRoot().getLeft().getData(), 25);
-		assertEquals((int) tree.getRoot().getRight().getData(), 50);
-		assertEquals((int) tree.getRoot().getLeft().getLeft().getData(), 13);
-		assertEquals((int) tree.getRoot().getLeft().getRight().getData(), 30);
-		assertEquals((int) tree.getRoot().getRight().getRight().getData(), 75);
+		assertEquals(37, (int) tree.getRoot().getData());
+		assertEquals(25, (int) tree.getRoot().getLeft().getData());
+		assertEquals(50, (int) tree.getRoot().getRight().getData());
+		assertEquals(13, (int) tree.getRoot().getLeft().getLeft().getData());
+		assertEquals(30, (int) tree.getRoot().getLeft().getRight().getData());
+		assertEquals(75, (int) tree.getRoot().getRight().getRight().getData());
 	}
 
 	@Test
@@ -91,13 +91,37 @@ public class AVLTest {
 		tree.add(50);
 		tree.add(75);
 		tree.add(37);
-		tree.add(30);
-		assertEquals((int) tree.getRoot().getData(), 37);
-		assertEquals((int) tree.getRoot().getLeft().getData(), 25);
-		assertEquals((int) tree.getRoot().getRight().getData(), 50);
-		assertEquals((int) tree.getRoot().getLeft().getLeft().getData(), 13);
-		assertEquals((int) tree.getRoot().getLeft().getRight().getData(), 30);
-		assertEquals((int) tree.getRoot().getRight().getRight().getData(), 75);
+		tree.add(38);
+		assertEquals(37, (int) tree.getRoot().getData());
+		assertEquals(25, (int) tree.getRoot().getLeft().getData());
+		assertEquals(50, (int) tree.getRoot().getRight().getData());
+		assertEquals(13, (int) tree.getRoot().getLeft().getLeft().getData());
+		assertEquals(38, (int) tree.getRoot().getRight().getLeft().getData());
+		assertEquals(75, (int) tree.getRoot().getRight().getRight().getData());
+	}
+
+	@Test
+	public void testSimpleRightLeft() {
+		AVL<Integer> tree = new AVL<Integer>();
+		tree.add(50);
+		tree.add(75);
+		tree.add(60);
+
+		assertEquals(60, (int) tree.getRoot().getData());
+		assertEquals(75, (int) tree.getRoot().getRight().getData());
+		assertEquals(50, (int) tree.getRoot().getLeft().getData());
+	}
+
+	@Test
+	public void testSimpleLeftRight() {
+		AVL<Integer> tree = new AVL<Integer>();
+		tree.add(50);
+		tree.add(25);
+		tree.add(37);
+
+		assertEquals(37, (int) tree.getRoot().getData());
+		assertEquals(25, (int) tree.getRoot().getLeft().getData());
+		assertEquals(50, (int) tree.getRoot().getRight().getData());
 	}
 
 	/**
