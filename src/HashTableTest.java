39fc1399a8e48a520fbbb94122917973010f639e
Jmoney4769 <jaredmoore47@gmail.com>
2013-02-20 20:36:05 -0500

    Added test for replacing an entry

--- src/HashTableTest.java
+++ src/HashTableTest.java
@@ -51,10 +51,10 @@ public class HashTableTest {
 				"[null, one, two, three, four, null, six, seven, eight, null, null, null, one again, null, null, null, null, null, null, null, null, null, null]",
 				table.toString());
 	}
-	
+
 	@Test
 	public void testWrapAround() {
-		
+
 		HashTable<Integer, String> table = new HashTable<Integer, String>();
 		table.put(1, "one");
 		table.put(2, "two");
@@ -63,12 +63,12 @@ public class HashTableTest {
 		table.put(12, "one again");
 		table.put(10, "ten");
 		table.put(21, "ten again");
-		
+
 		assertEquals(
 				"[ten again, one, two, three, four, one again, null, null, null, null, ten]",
 				table.toString());
 	}
-	
+
 	@Test
 	public void testReplace() {
 		HashTable<Integer, String> table = new HashTable<Integer, String>();
@@ -82,10 +82,10 @@ public class HashTableTest {
 				"[null, one again, two, three, four, null, null, null, null, null, null]",
 				table.toString());
 	}
-	
+
 	@Test
 	public void testGet() {
-		
+
 		HashTable<Integer, String> table = new HashTable<Integer, String>();
 		table.put(1, "one");
 		table.put(2, "two");
@@ -94,14 +94,14 @@ public class HashTableTest {
 		table.put(12, "one again");
 		table.put(10, "ten");
 		table.put(21, "ten again");
-		
+
 		assertEquals("ten", table.get(10));
 		assertEquals("ten again", table.get(21)); // same hash value as previous
 	}
-	
+
 	@Test
 	public void testContains() {
-		
+
 		HashTable<Integer, String> table = new HashTable<Integer, String>();
 		table.put(1, "one");
 		table.put(2, "two");
@@ -111,9 +111,31 @@ public class HashTableTest {
 		table.put(10, "ten");
 		table.put(21, "ten again");
 		table.put(7, null);
-		
+
 		assertEquals(true, table.containsKey(10));
 		assertEquals(true, table.containsKey(21));
 		assertEquals(true, table.containsKey(7));
+	}
+
+	@Test
+	public void testRemove() {
+		HashTable<Integer, String> table = new HashTable<Integer, String>();
+		table.put(1, "one");
+		table.put(2, "two");
+		table.put(3, "three");
+		table.put(4, "four");
+		table.put(12, "one again");
+
+		table.remove(1);
+		assertEquals(
+				"[null, hidden, two, three, four, one again, null, null, null, null, null]",
+				table.toString()); // my toString prints "hidden" if the entry
+									// has been removed
+		
+		table.put(5, null);
+		table.remove(5);
+		assertEquals(
+				"[null, hidden, two, three, four, one again, null, null, null, null, null]",
+				table.toString());
 	}
 }
