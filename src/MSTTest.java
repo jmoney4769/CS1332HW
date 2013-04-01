f89bd40fa7b5dd9879fd13c95266c914d08ee1f6
Jmoney4769 <jaredmoore47@gmail.com>
2013-04-01 00:09:27 -0400

    Added changes made by Taylor Wrodel

--- src/MSTTest.java
+++ src/MSTTest.java
@@ -1,79 +1,102 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- * @author Jared Moore
+ * @author Jared Moore edits done by @author Taylor Wrobel, see commit data and
+ *         version history for specifics
  * @version Mar 30, 2013
  */
 public class MSTTest {
 
-	public ArrayList<Graph> makeGraphList() {
-		
-		ArrayList<Graph> graphs = new ArrayList<Graph>();
+	private static Map<GraphType, Graph> graphs; // Changed to Map for
+													// readability. See test
+													// code for example.
+
+	private enum GraphType {
+		TWO_VERTEX, TWO_UNCONNECTED_SETS, TRIANGLE, WIKIPEDIA_EXAMPLE
+		// Enums represent graph type in clearer fashion
+	}
+
+	@BeforeClass
+	public static void makeGraphList() {
+
+		// Runs before all of the tests in this class. Since I/O reads have the
+		// potential to be time expensive, it's best to not do it at the start
+		// of every test, but rather once for the suite. This is fine in this
+		// case, because the list is never modified once it is created.
+
+		graphs = new HashMap<GraphType, Graph>();
 		String text = "";
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("res/graphs.txt"));
+			BufferedReader br = new BufferedReader(new FileReader(
+					"res/graphs.txt"));
 			text = br.readLine();
 			br.close();
-		}
-		catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			System.err.println("File not found");
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.err.println("Error with file");
 		}
-		
+
 		Scanner input = new Scanner(text);
 		input.useDelimiter("#");
 		String currentGraph;
+		int index = 0;
 		while (input.hasNext()) {
 			currentGraph = input.next();
-			graphs.add(new Graph(currentGraph));
+			// Enum ordering must match up with file ordering.
+			graphs.put(GraphType.values()[index], new Graph(currentGraph));
+			index++;
 		}
-	
+
 		input.close();
-		return graphs;
 	}
-	
+
 	@Test
 	public void testSimpleTwoVertex() {
-		ArrayList<Graph> graphs = makeGraphList();
-		Collection<Edge> edgeList = MST.kruskals(graphs.get(0));
+		Collection<Edge> edgeList = MST.kruskals(graphs
+				.get(GraphType.TWO_VERTEX)); // Much improved readability.
 		assertNotNull(edgeList);
 		assertEquals(1, edgeList.size());
 		assertTrue(edgeList.contains(new Edge(new Vertex(1), new Vertex(2), 1)));
 	}
-	
+
 	@Test
 	public void testSimpleNotConnected() {
-		ArrayList<Graph> graphs = makeGraphList();
-		Collection<Edge> edgeList = MST.kruskals(graphs.get(1));
+		Collection<Edge> edgeList = MST.kruskals(graphs
+				.get(GraphType.TWO_UNCONNECTED_SETS));
 		assertNull(edgeList);
 	}
-	
+
 	@Test
 	public void testSimpleTriangle() {
-		ArrayList<Graph> graphs = makeGraphList();
-		Collection<Edge> edgeList = MST.kruskals(graphs.get(2));
+		Collection<Edge> edgeList = MST
+				.kruskals(graphs.get(GraphType.TRIANGLE));
 		assertNotNull(edgeList);
 		assertEquals(2, edgeList.size());
 		assertTrue(edgeList.contains(new Edge(new Vertex(1), new Vertex(2), 1)));
 		assertTrue(edgeList.contains(new Edge(new Vertex(1), new Vertex(3), 2)));
 	}
-	
+
 	@Test
 	public void testWikipediaExample() {
-		ArrayList<Graph> graphs = makeGraphList();
-		Collection<Edge> edgeList = MST.kruskals(graphs.get(3));
+		Collection<Edge> edgeList = MST.kruskals(graphs
+				.get(GraphType.WIKIPEDIA_EXAMPLE));
 		assertNotNull(edgeList);
 		assertEquals(6, edgeList.size());
 		assertTrue(edgeList.contains(new Edge(new Vertex(1), new Vertex(2), 7)));
@@ -82,5 +105,13 @@ public class MSTTest {
 		assertTrue(edgeList.contains(new Edge(new Vertex(5), new Vertex(3), 5)));
 		assertTrue(edgeList.contains(new Edge(new Vertex(7), new Vertex(5), 9)));
 		assertTrue(edgeList.contains(new Edge(new Vertex(4), new Vertex(6), 6)));
+	}
+
+	@AfterClass
+	public static void cleanup() {
+		// Not necessary since this test class is standalone, but good practice.
+		// Frees up the graphs list to be garbage collected before other tests
+		// run, if this were part of a test suite.
+		graphs = null;
 	}
 }
