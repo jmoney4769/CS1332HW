16cc5d35c9628a9117317922c74720ba1a162a4d
Jmoney4769 <jaredmoore47@gmail.com>
2013-04-03 18:54:08 -0400

    Added the rest of the test cases

--- src/StringSearchesTest.java
+++ src/StringSearchesTest.java
@@ -40,8 +40,8 @@ public class StringSearchesTest {
 					array.add(input.next());
 				input.close();
 
-				for (int i = 0; i < array.size(); i++)
-					array.get(i).replaceAll("_", " ");
+				for (int i = 0; i < array.size(); i++) // all underscores will be treated as spaces
+					array.set(i, array.get(i).replace('_', ' '));
 
 				switch (lineNumber) {
 				case 5:
@@ -94,10 +94,14 @@ public class StringSearchesTest {
 	public void testFoxBoyerMoore() {
 
 		int first[] = { 10 }, second[] = new int[0], third[] = { 33 }, fourth[] = new int[0];
-		assertArrayEquals(first, StringSearches.boyerMoore(fox[1], fox[0]));
-		assertArrayEquals(second, StringSearches.boyerMoore(fox[2], fox[0]));
-		assertArrayEquals(third, StringSearches.boyerMoore(fox[3], fox[0]));
-		assertArrayEquals(fourth, StringSearches.boyerMoore(fox[4], fox[0]));
+		int firstSearch[] = StringSearches.boyerMoore(fox[1], fox[0]),
+				secondSearch[] = StringSearches.boyerMoore(fox[2], fox[0]),
+				thirdSearch[] = StringSearches.boyerMoore(fox[3], fox[0]), 
+				fourthSearch[] = StringSearches.boyerMoore(fox[4], fox[0]);
+		assertArrayEquals(first, firstSearch);
+		assertArrayEquals(second, secondSearch);
+		assertArrayEquals(third, thirdSearch);
+		assertArrayEquals(fourth, fourthSearch);
 	}
 
 	@Test
@@ -118,9 +122,88 @@ public class StringSearchesTest {
 	public void testFoxRabinKarp() {
 
 		int first[] = { 10 }, second[] = new int[0], third[] = { 33 }, fourth[] = new int[0];
-		assertArrayEquals(first, StringSearches.rabinKarp(fox[1], fox[0]));
-		assertArrayEquals(second, StringSearches.rabinKarp(fox[2], fox[0]));
-		assertArrayEquals(third, StringSearches.rabinKarp(fox[3], fox[0]));
-		assertArrayEquals(fourth, StringSearches.rabinKarp(fox[4], fox[0]));
+		int firstSearch[] = StringSearches.rabinKarp(fox[1], fox[0]),
+				secondSearch[] = StringSearches.rabinKarp(fox[2], fox[0]),
+				thirdSearch[] = StringSearches.rabinKarp(fox[3], fox[0]), 
+				fourthSearch[] = StringSearches.rabinKarp(fox[4], fox[0]);
+		assertArrayEquals(first, firstSearch);
+		assertArrayEquals(second, secondSearch);
+		assertArrayEquals(third, thirdSearch);
+		assertArrayEquals(fourth, fourthSearch);
+	}
+	
+	@Test
+	public void testDogBoyerMoore() {
+		int first[] = { 4, 8, 12, 16, 20, 24, 28 }, second[] = { 0 };
+		int firstSearch[] = StringSearches.boyerMoore(dog[1], dog[0]),
+				secondSearch[] = StringSearches.boyerMoore(dog[2], dog[0]);
+		assertArrayEquals(first, firstSearch);
+		assertArrayEquals(second, secondSearch);
+	}
+	
+	@Test
+	public void testDogKMP() {
+		int first[] = { 4, 8, 12, 16, 20, 24, 28 }, second[] = { 0 };
+		int firstSearch[] = StringSearches.kmp(dog[1], dog[0]),
+				secondSearch[] = StringSearches.kmp(dog[2], dog[0]);
+		assertArrayEquals(first, firstSearch);
+		assertArrayEquals(second, secondSearch);
+	}
+	
+	@Test
+	public void testDogRabinKarp() {
+		int first[] = { 4, 8, 12, 16, 20, 24, 28 }, second[] = { 0 };
+		int firstSearch[] = StringSearches.rabinKarp(dog[1], dog[0]),
+				secondSearch[] = StringSearches.rabinKarp(dog[2], dog[0]);
+		assertArrayEquals(first, firstSearch);
+		assertArrayEquals(second, secondSearch);
+	}
+	
+	@Test
+	public void testLettersBoyerMoore() {
+		int first[] = { 14 };
+		int firstSearch[] = StringSearches.boyerMoore(letters[1], letters[0]);
+		assertArrayEquals(first, firstSearch);
+	}
+	
+	@Test
+	public void testLettersKMP() {
+		int first[] = { 14 };
+		int firstSearch[] = StringSearches.kmp(letters[1], letters[0]);
+		assertArrayEquals(first, firstSearch);
+	}
+	
+	@Test
+	public void testLettersRabinKarp() {
+		int first[] = { 14 };
+		int firstSearch[] = StringSearches.rabinKarp(letters[1], letters[0]);
+		assertArrayEquals(first, firstSearch);
+	}
+	
+	@Test
+	public void testAardvarksBoyerMoore() {
+		int first[] = { 14 }, second[] = { 17 };
+		int firstSearch[] = StringSearches.boyerMoore(firstAardvark[1], firstAardvark[0]);
+		int secondSearch[] = StringSearches.boyerMoore(secondAardvark[1], secondAardvark[0]);
+		assertArrayEquals(first, firstSearch);
+		assertArrayEquals(second, secondSearch);
+	}
+	
+	@Test
+	public void testAardvarksKMP() {
+		int first[] = { 14 }, second[] = { 17 };
+		int firstSearch[] = StringSearches.kmp(firstAardvark[1], firstAardvark[0]);
+		int secondSearch[] = StringSearches.kmp(secondAardvark[1], secondAardvark[0]);
+		assertArrayEquals(first, firstSearch);
+		assertArrayEquals(second, secondSearch);
+	}
+	
+	@Test
+	public void testAardvarksRabinKarp() {
+		int first[] = { 14 }, second[] = { 17 };
+		int firstSearch[] = StringSearches.rabinKarp(firstAardvark[1], firstAardvark[0]);
+		int secondSearch[] = StringSearches.rabinKarp(secondAardvark[1], secondAardvark[0]);
+		assertArrayEquals(first, firstSearch);
+		assertArrayEquals(second, secondSearch);
 	}
 }
